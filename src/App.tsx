648cbd4cfda7931983628f55b0d735e37c4b8eb3
Wilgot <wilgotwardell@hotmail.com>
2023-08-24 16:51:24 +0200

    -

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="w-[100%] h-[100px] bg-black"></div>
+    </div>
+  );
 }
 
 export default App;
